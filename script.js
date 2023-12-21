a120ecde9218690eacf5bf651f979764d3cdb400
Jayro <144926964+JayroSantos@users.noreply.github.com>
2023-12-21 20:31:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//efeito do botão voltar ao Topo
+
+function topo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
+
+//Validação de Login
+
+function Login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = 'index.html';
+        logado = 1;
+}
+
+     if(logado == 0){
+        alert("Acesso Negado. Dados Incorretos.")
+     }
+}    
+
+//Ativar alert no botão cadastrar
+function Cadastro(){
+alert("Cadastro com Sucesso");
+window.location.href = "index.html";
+}
